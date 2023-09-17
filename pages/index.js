d5216c3a6cac5e557b8563f7bdf0cd09cc06143e
Hebert Zanatelli <hebert_alves1@hotmail.com>
2023-09-17 19:25:51 -0300

    adicionando footer e refatorando container nos componentes

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,6 @@ import SearchIcon from '@material-ui/icons/search'
 import TemplateDefault from '../src/templates/Default'
 
 const useStyles = makeStyles((theme) => ({
-    searchContainer: {
-        padding: theme.spacing(8, 10, 6),
-
-    },
     searchBox: {
         display: 'flex',
         justifyContent: 'center',
@@ -42,7 +38,7 @@ export default function Home() {
 
     return (
         <TemplateDefault>
-            <Container maxWidth='md' className={classes.searchContainer}>
+            <Container maxWidth='md' >
                 <Typography component='h1' variant='h3' align='center' color='textPrimary'>
                     O que deseja encontrar?
                 </Typography>
